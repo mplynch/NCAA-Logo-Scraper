e920eab9f846b1ad73017c95a570a3f034e50ee5
Michael Lynch <mplynch@gmail.com>
2014-04-04 00:14:28 -0500

    Updated to run in Python 3 with BeautifulSoup4

--- pull_logos.py
+++ pull_logos.py
@@ -1,29 +1,40 @@
 from bs4 import BeautifulSoup
-import urllib
-import pprint
-import string
-import tempfile
+import io
+import urllib.request
 
 urls = [
-		'http://www.sportslogos.net/teams/list_by_league/30/NCAA_Division_I_a-c/NCAA_a-c/logos/',
-		'http://www.sportslogos.net/teams/list_by_league/31/NCAA_Division_I_d-h/NCAA_d-h/logos/',
-		'http://www.sportslogos.net/teams/list_by_league/32/NCAA_Division_I_i-m/NCAA_i-m/logos/',
-		'http://www.sportslogos.net/teams/list_by_league/33/NCAA_Division_I_n-r/NCAA_n-r/logos/',
-		'http://www.sportslogos.net/teams/list_by_league/34/NCAA_Division_I_s-t/NCAA_s-t/logos/',
-		'http://www.sportslogos.net/teams/list_by_league/35/NCAA_Division_I_u-z/NCAA_u-z/logos/'
+        'http://www.sportslogos.net/teams/list_by_league/30/NCAA_Division_I_a-c/NCAA_a-c/logos/',
+        'http://www.sportslogos.net/teams/list_by_league/31/NCAA_Division_I_d-h/NCAA_d-h/logos/',
+        'http://www.sportslogos.net/teams/list_by_league/32/NCAA_Division_I_i-m/NCAA_i-m/logos/',
+        'http://www.sportslogos.net/teams/list_by_league/33/NCAA_Division_I_n-r/NCAA_n-r/logos/',
+        'http://www.sportslogos.net/teams/list_by_league/34/NCAA_Division_I_s-t/NCAA_s-t/logos/',
+        'http://www.sportslogos.net/teams/list_by_league/35/NCAA_Division_I_u-z/NCAA_u-z/logos/'
 ]
+
+str = io.StringIO()
+str.write('[')
 
 for url in urls:
 
-	print 'Loading HTML...'
-	s = urllib.urlopen(url)
-	html = s.read()
+    print('Loading HTML...')
+    s = urllib.request.urlopen(url)
+    html = s.read()
 
-	soup = BeautifulSoup(html)
-	for li in soup.find_all('img'):
-		filename = unicode(li.string).strip().replace(' ','') + '.gif'
-		image_url = li['src']
+    soup = BeautifulSoup(html)
+    for li in soup.find_all('img'):
+        #print(li)
+        if li.string is None:
+            continue
+        team = li.string.strip()
+        filename = team.replace(' ','') + '.gif'
+        image_url = li['src']
 
-		print tempfile.gettempdir() # prints the current temporary directory
-		print 'Downloading ' + filename + '...'
-		urllib.urlretrieve(image_url, filename)
+        print('Downloading ' + filename + '...')
+        urllib.request.urlretrieve(image_url, filename)
+
+        str.write('{"name":"' + team + '", "logoPath":"/img/logos/' + filename + '"},\n')
+
+outputValue = str.getvalue().rstrip(',\n')
+outputValue = outputValue + "]"
+print('Writing teams.json')
+open('teams.json', 'wt').write(outputValue)
